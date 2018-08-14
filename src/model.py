4b7e97852daf27d4d499f856f503913d0eb776e4
Monica C Oyarzun <monicacoyarzun@Monicas-MacBook-Pro.local>
2018-08-13 17:07:06 -0700

    changed model specs

--- src/model.py
+++ src/model.py
@@ -19,12 +19,13 @@ scaler = MinMaxScaler(feature_range = (0,1))
 scaler.fit(subset)
 scaled_subset = scaler.transform(subset)
 
-# perform PCA to bring components down to 10
-svd = TruncatedSVD(n_components=10, n_iter=7)
+# perform PCA to bring components down to 8
+svd = TruncatedSVD(n_components=8, n_iter=7)
 X_svd = svd.fit_transform(scaled_subset)
 
 # kMeans model after PCA
-kmeans = KMeans(n_clusters=6)
+n_clusters = 9
+kmeans = KMeans(n_clusters)
 predictions = kmeans.fit_predict(X_svd)
 silhouette_score = silhouette_score(X_svd, predictions, metric='euclidean')
 print ('predictions: ', np.unique(predictions, return_counts=True))
@@ -33,7 +34,7 @@ print ('silhouette score: ', silhouette_score)
 # find top features from clustering
 centroids = svd.inverse_transform(kmeans.cluster_centers_)
 top_centroids = np.argsort(centroids)[:, -1:-11:-1]
-for i in range(6):
+for i in range(n_clusters):
     print ('Cluster', i+1, 'total: ', np.unique(predictions == i, return_counts=True)[1][1])
     print (subset[predictions == i].iloc[:, top_centroids[i]].columns)
     print ()
